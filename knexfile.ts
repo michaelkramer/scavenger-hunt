209376ae5fd36094720c74e59c83c9bba55bc354
Michael Kramer <michaelrkramer@gmail.com>
2020-06-16 16:44:27 -0400

    update db client

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,7 @@
 // Update with your config settings.
 require("dotenv").config();
 module.exports = {
-  client: "postgresql",
+  client: "pg",
   connection: process.env.DATABASE_URL, //"postgresql://db:db@127.0.0.1:5550/db",
   pool: {
     min: 2,
