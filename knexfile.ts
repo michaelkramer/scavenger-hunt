1ca2f0b50f871179138bed05a3b6b4fa36081174
Michael Kramer <michaelrkramer@gmail.com>
2020-06-16 14:15:17 -0400

    nothing

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,9 @@
 // Update with your config settings.
-
+const env = require("./src/env");
 module.exports = {
   development: {
     client: "postgresql",
-    connection: "postgresql://db:db@127.0.0.1:5550/db",
+    connection: env.env.db.connection, //"postgresql://db:db@127.0.0.1:5550/db",
     pool: {
       min: 2,
       max: 10,
