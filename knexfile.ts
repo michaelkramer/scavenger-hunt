1b057fd580f43b5317811c5b7e3207c22766040a
Michael Kramer <michaelrkramer@gmail.com>
2020-06-16 16:39:50 -0400

    change db

--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,13 @@
 // Update with your config settings.
-//const env = require("./src/env");
+require("dotenv").config();
 module.exports = {
-  development: {
-    client: "postgresql",
-    connection: process.env.DATABASE_URL, //"postgresql://db:db@127.0.0.1:5550/db",
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "migrations",
-    },
+  client: "postgresql",
+  connection: process.env.DATABASE_URL, //"postgresql://db:db@127.0.0.1:5550/db",
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "migrations",
   },
 };
